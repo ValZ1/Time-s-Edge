0058b82fe6017a0adad61a94340bd82dc4a9976f
ValZ1 <146456916+ValZ1@users.noreply.github.com>
2024-11-04 22:50:02 +0300

    Добавил руку игроку

---
+++ Time-s-Edge/Assets/Code/ArmPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ArmPlayer : MonoBehaviour
+{
+    private float angle;
+    private float currentVelocity;
+    public float maxTurnSpeed = 180;
+    public float smoothTime = 0.1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+        float targetAngle = Vector2.SignedAngle(Vector2.right, direction);
+        angle = Mathf.SmoothDampAngle(angle, targetAngle, ref currentVelocity, smoothTime, maxTurnSpeed);
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
