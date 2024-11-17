d7303566450dba92afda3f38a571c62d4bdd8f2c
isevo353 <vanasevostanov33@gmail.com>
2024-11-17 14:49:11 +0300

    hud

--- Time-s-Edge/Assets/Code/HP.cs
+++ Time-s-Edge/Assets/Code/HP.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 public class HP : MonoBehaviour
 {
     public int Hp;
+    public int Damage;
+    public float spdamage;
 
     public Player Player;
+    public Bullet Bullet;
     public TextMeshProUGUI Text;
 
     // Update is called once per frame
     void Update()
     {
+        Damage = Bullet.DamageBullet;
+        spdamage = Bullet.SpeedBullet;
         Hp = Player.CurHp;
-        Text.text = Hp.ToString();
+        Text.text = "HP " + Hp.ToString() + "\n" +"Damage "+ Damage.ToString()+"\n"+"Atakspped "+spdamage.ToString();
     }
 }
