fd95c9e692dcc377743ad15812021f8a5442e3f8
Zxctolas <nano.buster0@yandex.ru>
2025-05-04 13:59:40 +0300

    добавил комнату босса

--- Time-s-Edge/Assets/Code/Room.cs
+++ Time-s-Edge/Assets/Code/Room.cs
@@ -15,31 +15,46 @@ public class Room : MonoBehaviour
     private int _index;
     private int _countEnemy = 0;
     public bool is_bossroom = false;
+    public int cleared_rooms = 0;
 
     public bool isCurrentRoom = false;
     public bool _roomActive;
+    private bool cleared = false;
     void Start()
     {
-        _roomActive = true;
-        Waves = UnityEngine.Random.Range(2, 4);
-        OpenDoors();
+        if (!is_bossroom)
+        {
+            _roomActive = true;
+            Waves = 1;
+            OpenDoors();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cleared_rooms == 7)
+        {
+            _roomActive = true;
+            Waves = UnityEngine.Random.Range(2, 4);
+            OpenDoors();
+            cleared_rooms++;
+        }
     }
     private void OnEnemySpawnPoint()
     {
-        foreach (EnemySpawnPoint enemySpawnPoint in EnemySpawnPoints)
-        {
-            _index = UnityEngine.Random.Range(0, 5);
-            enemySpawnPoint.SpawnEnemy(_index);
-            _countEnemy++;
-        }
         if (is_bossroom)
         {
             Instantiate(prefabBoss, BossSpawnPoint.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            foreach (EnemySpawnPoint enemySpawnPoint in EnemySpawnPoints)
+            {
+                _index = UnityEngine.Random.Range(0, 5);
+                enemySpawnPoint.SpawnEnemy(_index);
+                _countEnemy++;
+            }
         }
     }
     private void CloseDoors()
@@ -54,6 +69,29 @@ public class Room : MonoBehaviour
         foreach (Door door in Doors)
         {
             door.Open();
+        }
+        if (cleared)
+        {
+            GameObject bossRoom = GameObject.Find("RoomTriggerBosS");
+            if (bossRoom != null)
+            {
+                // Достаём ваш скрипт Room с этого объекта
+                Room roomScript = bossRoom.GetComponent<Room>();
+                if (roomScript != null)
+                {
+                    // Увеличиваем счётчик
+                    roomScript.cleared_rooms++;
+                }
+                else
+                {
+                    Debug.LogError("На объекте 'RoomTriggerBosS' нет компонента Room!");
+                }
+            }
+            else
+            {
+                Debug.LogError("Не найден объект 'RoomTriggerBosS' в сцене!");
+            }
+            Destroy(gameObject);
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -78,6 +116,7 @@ public class Room : MonoBehaviour
             {
                 _roomActive = false;
                 //Тут можно реализовать волны
+                cleared = true;
                 OpenDoors();
             }
         }
